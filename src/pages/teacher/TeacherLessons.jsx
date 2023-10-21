882a7c66cbed7a254c31ab6bf2d3fda15882126d
Abdelwahab <ab28fb@gmail.com>
2023-10-21 23:41:34 +0100

    WIP/Feature 3: create a meet directly from lessons page by teachers

--- src/pages/teacher/TeacherLessons.jsx
+++ src/pages/teacher/TeacherLessons.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
-import { Container, Paper } from "@mui/material";
+import { Button, Container, Paper } from "@mui/material";
 import BookedLesson from "../../components/student/BookedLesson";
 import { useTeacherLessons } from "../../hooks/useTeacherLessons";
 import Loading from "../../components/Loading";
 import { useSelector } from "react-redux";
+import VideoCallIcon from "@mui/icons-material/VideoCall";
+import { useTranslation } from "react-i18next";
 
 export default function TeacherLessons() {
   const { teacher, token } = useSelector((state) => state.teacher);
   const { data, isLoading } = useTeacherLessons(teacher.id, token);
+  const { t } = useTranslation();
   return (
     <Navbar>
-      <Container sx={{ marginTop: "120px" }}>
+      <Container sx={{ marginTop: "120px", marginBottom: "80px" }}>
+        <Button
+          href="http://meet.google.com/new"
+          target="_blank"
+          variant="contained"
+          color="success"
+          size="medium"
+          endIcon={
+            <VideoCallIcon sx={{ marginRight: "16px", fontSize: "large" }} />
+          }
+        >
+          {t("start_lesson_meet")}
+        </Button>
         {!isLoading ? (
-          <Paper sx={{ marginY: "80px", padding: "40px 20px 20px" }}>
+          <Paper sx={{ marginY: "20px", padding: "40px 20px 20px" }}>
             {data?.data?.length > 0 &&
               data.data.map((lesson, index) => {
                 return (
