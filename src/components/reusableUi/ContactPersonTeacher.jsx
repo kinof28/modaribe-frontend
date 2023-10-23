bf783204be92229c102f7aee7698e10395067741
Abdelwahab <ab28fb@gmail.com>
2023-10-23 06:00:42 +0100

    Fix: Admin conversation title in clients

--- src/components/reusableUi/ContactPersonTeacher.jsx
+++ src/components/reusableUi/ContactPersonTeacher.jsx
@@ -1,17 +1,41 @@
-import { Avatar, Box, Button, Typography } from '@mui/material'
-import React from 'react'
-import { useStudent } from '../../hooks/useStudent';
+import { Avatar, Box, Button, Typography } from "@mui/material";
+import React from "react";
+import { useStudent } from "../../hooks/useStudent";
 
-export default function ContactPersonTeacher({item , selectChat,lastMessage,active}) {
-    const {data} = useStudent(item.studentId);
-    return (
-            <Box sx={{display:"flex",alignItems:"center",columnGap:"12px",marginBottom:"20px",
-            cursor:"pointer" , backgroundColor:active?"#eee":"" , padding:"12px 8px" , borderRadius:"6px"}} onClick={selectChat}>
-                <Avatar alt={data?.data?.name} src={`${process.env.REACT_APP_API_KEY}images/${data?.data?.image}`} sx={{width:"45px",height:"45px"}}/>
-                <Box>
-                    <Typography sx={{margin:0}}>{data?.data?.name}</Typography>
-                    <Typography sx={{fontSize:"12px"}}>{lastMessage?.text?.slice(0.30)}</Typography>
-                </Box>
-            </Box>
-    )
+export default function ContactPersonTeacher({
+  item,
+  selectChat,
+  lastMessage,
+  active,
+}) {
+  const { data } = useStudent(item.studentId);
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        columnGap: "12px",
+        marginBottom: "20px",
+        cursor: "pointer",
+        backgroundColor: active ? "#eee" : "",
+        padding: "12px 8px",
+        borderRadius: "6px",
+      }}
+      onClick={selectChat}
+    >
+      <Avatar
+        alt={data?.data?.name}
+        src={`${process.env.REACT_APP_API_KEY}images/${data?.data?.image}`}
+        sx={{ width: "45px", height: "45px" }}
+      />
+      <Box>
+        <Typography sx={{ margin: 0 }}>
+          {item.studentId === "0" ? "Admin" : data?.data?.name}
+        </Typography>
+        <Typography sx={{ fontSize: "12px" }}>
+          {lastMessage?.text?.slice(0.3)}
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
