8203bdef7c371c6c45bff0164ab5309d56e4aed4
Abdelwahab <ab28fb@gmail.com>
2023-11-20 12:31:11 +0100

    Fix: server link from.env

--- src/hooks/useStudentMap.js
+++ src/hooks/useStudentMap.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 
 async function getMap(id, token) {
   const response = await fetch(
-    `https://server.moalime.com/api/v1/student/nearestTeachers/${id}`,
+    `${process.env.REACT_APP_API_KEY}api/v1/student/nearestTeachers/${id}`,
     {
       method: "GET",
       headers: {
