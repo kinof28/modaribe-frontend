e7c390db6ca65dfd8302c2faaac403908311388a
Abdelwahab <ab28fb@gmail.com>
2023-11-21 02:33:33 +0100

    Feature Init: add new teacher or student

---
+++ src/pages/admin/AdminNewStudent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AdminLayout from "../../components/admin/AdminLayout";
+
+const AdminNewStudent = () => {
+  return <AdminLayout></AdminLayout>;
+};
+
+export default AdminNewStudent;
