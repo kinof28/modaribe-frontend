5789bdb655437f0c7842f3381f70b6abcaedb46a
Abdelwahab <ab28fb@gmail.com>
2023-11-21 13:28:21 +0100

    Feature UI: admin can see checkout requests

---
+++ src/hooks/useCheckouts.js
@@ -0,0 +1,35 @@
+import { useQuery } from "react-query";
+
+async function getNewCheckouts(token) {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_KEY}api/v1/admin/getNewCheckoutRequests`,
+    {
+      headers: {
+        Authorization: token,
+      },
+    }
+  );
+  return response.json();
+}
+
+export const useNewCheckouts = (token) => {
+  return useQuery("get-new-checkouts", () => getNewCheckouts(token));
+};
+
+async function getProcessedCheckouts(token) {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_KEY}api/v1/admin/getProcessedCheckoutRequests`,
+    {
+      headers: {
+        Authorization: token,
+      },
+    }
+  );
+  return response.json();
+}
+
+export const useParentStudentFinished = (token) => {
+  return useQuery("get-processed-checkouts", () =>
+    getProcessedCheckouts(token)
+  );
+};
--- src/pages/admin/AdminCheckoutRequests.jsx
+++ src/pages/admin/AdminCheckoutRequests.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import AdminLayout from "../../components/admin/AdminLayout";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Box, Tab } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import ParentStudentNew from "../../components/admin/ParentStudentNew";
+import ParentStudentFinished from "../../components/admin/ParentStudentFinished";
+import CheckoutsNew from "../../components/admin/CheckoutsNew";
+import CheckoutsProcessed from "../../components/admin/CheckoutsProcessed";
 
-const AdminCheckoutRequests = () => {
-  return <AdminLayout></AdminLayout>;
-};
-
-export default AdminCheckoutRequests;
+export default function AdminCheckoutRequests() {
+  const { t } = useTranslation();
+  const [value, setValue] = React.useState("1");
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <AdminLayout>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label={t("new")} value="1" />
+            <Tab label={t("finished")} value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <CheckoutsNew />
+        </TabPanel>
+        <TabPanel value="2">
+          <CheckoutsProcessed />
+        </TabPanel>
+      </TabContext>
+    </AdminLayout>
+  );
+}
