31fb3e0bf10cc3c61bf73c827c6aa23a8b69eb27
Abdelwahab <ab28fb@gmail.com>
2024-01-02 13:01:41 +0100

    Feature: Display a message for teacher about his account state

--- src/components/teacher/TeacherLayout.jsx
+++ src/components/teacher/TeacherLayout.jsx
@@ -1,10 +1,64 @@
 import { Container, Paper, Typography } from "@mui/material";
 import React from "react";
 import TeacherStepper from "./TeacherStepper";
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import { useTeacher } from "../../hooks/useTeacher";
 
 export default function TeacherLayout({ active, title, children }) {
+  const { t } = useTranslation();
+  const { teacher } = useSelector((state) => state.teacher);
+  const { data, isLoading } = useTeacher(teacher?.id);
   return (
     <Container sx={{ marginBottom: "60px", marginTop: "120px" }}>
+      {!isLoading &&
+      (!data.data.firstName ||
+        !data.data.lastName ||
+        !data.data.gender ||
+        !data.data.dateOfBirth ||
+        !data.data.phone ||
+        !data.data.email ||
+        !data.data.country ||
+        !data.data.city ||
+        !data.data.image ||
+        !data.data.favStdGender ||
+        !data.data.experienceYears ||
+        !data.data.favHours ||
+        !data.data.shortHeadlineAr ||
+        !data.data.shortHeadlineEn ||
+        !data.data.descriptionAr ||
+        !data.data.descriptionEn ||
+        data.data.CurriculumTeachers.length === 0 ||
+        data.data.TeacherLevels.length === 0) ? (
+        <Typography
+          sx={{
+            bgcolor: "#f50000",
+            padding: "10px 20px",
+            color: "#fff",
+            fontWeight: "600",
+            borderRadius: "10px",
+          }}
+          variant="h5"
+        >
+          {t("complete_teacher_account")}
+        </Typography>
+      ) : (
+        !data.data.isVerified && (
+          <Typography
+            sx={{
+              bgcolor: "orange",
+              padding: "10px 20px",
+              color: "#fff",
+              fontWeight: "600",
+              borderRadius: "10px",
+            }}
+            variant="h5"
+          >
+            {t("account_under_review")}
+          </Typography>
+        )
+      )}
+
       <TeacherStepper active={active} />
       <Paper sx={{ marginY: "50px", paddingY: "40px", paddingX: "30px" }}>
         <Typography
