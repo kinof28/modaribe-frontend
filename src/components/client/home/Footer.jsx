1d16b18bfcbf13aa6e60f4f7a4b630906a3fc168
Abdelwahab <ab28fb@gmail.com>
2024-05-03 09:39:47 +0100

    Fix: Changing logo color

--- src/components/client/home/Footer.jsx
+++ src/components/client/home/Footer.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import {Box, Container,styled, Typography} from '@mui/material'
-import logo from '../../../images/logo.png'
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { Box, Container, styled, Typography } from "@mui/material";
+import logo from "../../../images/logo1.png";
+import { useTranslation } from "react-i18next";
 
-const Image = styled('img')({
-    width:"120px",
-    height:"60px"
-})
+const Image = styled("img")({
+  width: "120px",
+  height: "60px",
+});
 
 export default function Footer() {
-    const {t} = useTranslation()
-    return (
-        <Box>
-            <Container sx={{marginY:"30px",display:"flex",justifyContent:{xs:"center",sm:"space-between"},
-            alignItems:"center",flexDirection:{xs:"column",sm:"row"},gap:"12px"}}>
-                <Image src={logo}/>
-                <Typography sx={{color:"#6D6D6D",fontSize:"15px"}}>
-                    {t('privacy')}      
-                </Typography>
-            </Container>
-        </Box>
-    )
+  const { t } = useTranslation();
+  return (
+    <Box>
+      <Container
+        sx={{
+          marginY: "30px",
+          display: "flex",
+          justifyContent: { xs: "center", sm: "space-between" },
+          alignItems: "center",
+          flexDirection: { xs: "column", sm: "row" },
+          gap: "12px",
+        }}
+      >
+        <Image src={logo} />
+        <Typography sx={{ color: "#6D6D6D", fontSize: "15px" }}>
+          {t("privacy")}
+        </Typography>
+      </Container>
+    </Box>
+  );
 }
