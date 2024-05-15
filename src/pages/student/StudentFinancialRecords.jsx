fe6b9463f44c35b129d7810485269c3d4aeae04d
Abdelwahab <ab28fb@gmail.com>
2024-05-15 17:10:00 +0100

    Feature: add financial records to the payment history

--- src/pages/student/StudentFinancialRecords.jsx
+++ src/pages/student/StudentFinancialRecords.jsx
@@ -16,12 +16,15 @@ import {
 } from "@mui/material";
 import moment from "moment";
 import Loading from "../../components/Loading";
+import Moment from "moment";
+import { useStudentHistory } from "../../hooks/useStudentHistory";
 
 function StudentFinancialRecords() {
   const lang = Cookies.get("i18next") || "en";
   const { t } = useTranslation();
   const { student, token } = useSelector((state) => state.student);
   const { data, isLoading } = useStudentFinancialRecords(student.id, token);
+  const { data: data2, isLoading: isLoading2 } = useStudentHistory(student.id);
   return (
     <StudentLayout>
       {!isLoading ? (
@@ -95,6 +98,62 @@ function StudentFinancialRecords() {
                         {moment(row.createdAt).format(
                           "MMMM Do YYYY, h:mm:ss a"
                         )}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </Box>
+        </Paper>
+      ) : (
+        <Loading />
+      )}
+      {!isLoading2 ? (
+        <Paper sx={{ padding: "20px", marginBottom: "40px" }}>
+          <Typography
+            sx={{
+              fontSize: "24px",
+              marginTop: "12px",
+              fontWeight: "600",
+              marginBottom: "30px",
+            }}
+          >
+            {t("payment_history")}
+          </Typography>
+          <Box sx={{ overflow: "auto" }}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="right" sx={{ color: "#005B8E" }}>
+                    {t("history")}
+                  </TableCell>
+                  <TableCell align="right" sx={{ color: "#005B8E" }}>
+                    {t("currency")}
+                  </TableCell>
+                  <TableCell align="right" sx={{ color: "#005B8E" }}>
+                    {t("amount")}
+                  </TableCell>
+                  <TableCell align="right" sx={{ color: "#005B8E" }}>
+                    {t("status")}
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data2?.data.length > 0 &&
+                  data2?.data.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell align="right">
+                        {Moment(row.createdAt).format(
+                          "MMMM Do YYYY, h:mm:ss a"
+                        )}
+                      </TableCell>
+                      <TableCell align="right">{row.currency}</TableCell>
+                      <TableCell align="right">{row.price}</TableCell>
+                      <TableCell align="right">
+                        {lang === "ar" ? row.typeAr : row.typeEn}
                       </TableCell>
                     </TableRow>
                   ))}
