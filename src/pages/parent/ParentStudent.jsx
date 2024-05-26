e0b5da5f613cf27c88d797c413c6695daad9763a
Abdelwahab <ab28fb@gmail.com>
2024-05-26 16:55:29 +0100

    feature: add link to go back

--- src/pages/parent/ParentStudent.jsx
+++ src/pages/parent/ParentStudent.jsx
@@ -1,43 +1,90 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import { Container,Paper, Typography } from '@mui/material'
-import { useParams } from 'react-router-dom'
-import {useAllLessons} from '../../hooks/useAllLessons'
-import BookedLesson from '../../components/student/BookedLesson'
-import Loading from '../../components/Loading'
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import Navbar from "../../components/Navbar";
+import {
+  Box,
+  Container,
+  Grid,
+  Paper,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
+import { Link, useParams } from "react-router-dom";
+import { useAllLessons } from "../../hooks/useAllLessons";
+import BookedLesson from "../../components/student/BookedLesson";
+import Loading from "../../components/Loading";
+import { useTranslation } from "react-i18next";
 
 export default function ParentStudent() {
-    const {id} = useParams()
-    const {data,isLoading} = useAllLessons(id)
-    const {t} = useTranslation() 
+  const { id } = useParams();
+  const { data, isLoading } = useAllLessons(id);
+  const { t } = useTranslation();
 
-    return (
-        <Navbar>
-            <Container sx={{marginTop:"120px"}}>
-            {
-                !isLoading?
-                <Paper sx={{marginY:"80px",padding:"40px 20px 20px"}}>
-                    <Typography sx={{marginBottom:"30px",fontSize:"24px",fontWeight:"600"}}>{t('registeredlessons')}</Typography>
-                {
-                    data?.data?.length>0&&data.data.map((lesson,index)=>
-                    {
-                        return(
-                            <BookedLesson
-                            image={lesson?.Teacher?.image}
-                            name={lesson?.Teacher.firstName+" "+lesson?.Teacher.lastName}
-                            date={lesson?.date}
-                            type={lesson?.type}
-                            period={lesson?.period}
-                            key={index+'zw3'}/>
-                        )
-                    })
-                }
-                </Paper>
-                :
-                <Loading/>
-            }
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar>
+      <Container sx={{ marginTop: "120px" }}>
+        <Typography
+          sx={{
+            color: "#000000",
+            fontWeight: "700",
+            fontSize: "24px",
+            marginBottom: "8px",
+          }}
+        >
+          {t("parent_welcome")}
+        </Typography>
+        <Typography
+          sx={{
+            color: "#000000",
+            fontWeight: "500",
+            fontSize: "18px",
+            marginBottom: "32px",
+          }}
+        >
+          {t("parent_desc")}
+        </Typography>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Link to={"/parent"}>
+            <Tabs value="1">
+              <Tab label={t("view_children")} value="1" />
+            </Tabs>
+          </Link>
+        </Box>
+        <Container>
+          {!isLoading ? (
+            <Paper sx={{ marginY: "20px", padding: "40px 20px 20px" }}>
+              <Typography
+                sx={{
+                  marginBottom: "30px",
+                  fontSize: "24px",
+                  fontWeight: "600",
+                }}
+              >
+                {t("registeredlessons")}
+              </Typography>
+              {data?.data?.length > 0 &&
+                data.data.map((lesson, index) => {
+                  return (
+                    <BookedLesson
+                      image={lesson?.Teacher?.image}
+                      name={
+                        lesson?.Teacher.firstName +
+                        " " +
+                        lesson?.Teacher.lastName
+                      }
+                      date={lesson?.date}
+                      type={lesson?.type}
+                      period={lesson?.period}
+                      key={index + "zw3"}
+                    />
+                  );
+                })}
+            </Paper>
+          ) : (
+            <Loading />
+          )}
+        </Container>
+      </Container>
+    </Navbar>
+  );
 }
