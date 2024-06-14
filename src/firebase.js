371a0fb8832e89e19b3454d7325a740e09133c02
Abdelwahab <ab28fb@gmail.com>
2024-06-14 18:34:09 +0100

    FIX: change firebase conf

--- src/firebase.js
+++ src/firebase.js
@@ -1,31 +1,20 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBit8WdqD7OCkh6JA1p80V6afXMBVAjQo4",
-//   authDomain: "chat-app-yt-87979.firebaseapp.com",
-//   projectId: "chat-app-yt-87979",
-//   storageBucket: "chat-app-yt-87979.appspot.com",
-//   messagingSenderId: "629968309261",
-//   appId: "1:629968309261:web:00539b2aa1847a17ad9944"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const db = getFirestore();
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDgCmRAtjY-jqVRmHb4W_dlmkJoZnwbZs8",
-  authDomain: "moalemy-54d19.firebaseapp.com",
-  projectId: "moalemy-54d19",
-  storageBucket: "moalemy-54d19.appspot.com",
-  messagingSenderId: "239715172398",
-  appId: "1:239715172398:web:79534f914a21f869c11fbf",
-  measurementId: "G-51Y8PNXGK8"
+  // apiKey: "AIzaSyDgCmRAtjY-jqVRmHb4W_dlmkJoZnwbZs8",
+  // authDomain: "moalemy-54d19.firebaseapp.com",
+  // projectId: "moalemy-54d19",
+  // storageBucket: "moalemy-54d19.appspot.com",
+  // messagingSenderId: "239715172398",
+  // appId: "1:239715172398:web:79534f914a21f869c11fbf",
+  // measurementId: "G-51Y8PNXGK8"
+  apiKey: "AIzaSyCLURQd0SBvd4iZJ9JcFZJXin4h-iDrE_s",
+  authDomain: "muscat-driving-school.firebaseapp.com",
+  projectId: "muscat-driving-school",
+  storageBucket: "muscat-driving-school.appspot.com",
+  messagingSenderId: "524301110380",
+  appId: "1:524301110380:web:81e32db0c0b8deea271c49",
 };
 
 const app = initializeApp(firebaseConfig);
